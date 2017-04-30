d2714ab5d3da99766172eb19777e7322ad1b011c
Shiladitya Mandal <16.shiladitya@gmail.com>
2017-04-30 14:42:07 +0530

    init commit

---
+++ lib/grpc/client/commons.rb
@@ -0,0 +1,9 @@
+require "grpc/client/commons/version"
+
+module Grpc
+  module Client
+    module Commons
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/grpc/client/commons/version.rb
@@ -0,0 +1,7 @@
+module Grpc
+  module Client
+    module Commons
+      VERSION = "0.1.0"
+    end
+  end
+end
